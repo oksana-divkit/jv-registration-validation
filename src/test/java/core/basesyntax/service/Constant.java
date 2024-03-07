444ae4ce2a773dfa640e4e66de376a02d67f1c28
Oksana Miazina <oksana.myazina@gmail.com>
2024-03-07 17:43:13 +0200

    fix: improve tests

---
+++ src/test/java/core/basesyntax/service/Constant.java
@@ -0,0 +1,22 @@
+package core.basesyntax.service;
+
+public class Constant {
+    public static final String VALID_LOGIN = "123456";
+    public static final String VALID_LOGIN2 = "12345678";
+    public static final String VALID_PASSWORD = "123456";
+    public static final String VALID_PASSWORD2 = "12345678";
+    public static final int VALID_AGE = 18;
+    public static final int VALID_AGE2 = 36;
+
+    public static final String INVALID_LOGIN = "";
+    public static final String INVALID_LOGIN2 = "123";
+    public static final String INVALID_PASSWORD = "";
+    public static final String INVALID_PASSWORD2 = "123";
+    public static final String INVALID_PASSWORD3 = "12345";
+    public static final int INVALID_AGE = 17;
+    public static final int INVALID_AGE2 = -17;
+
+    public static final int MIN_LENGTH_LOGIN = 6;
+    public static final int MIN_LENGTH_PASSWORD = 6;
+    public static final int MIN_AGE = 18;
+}
