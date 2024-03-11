6232747e11982b3d835ca9ae23ff7f650296864b
Oksana Miazina <oksana.myazina@gmail.com>
2024-03-11 15:45:20 +0200

    refactor: improve tests

--- src/test/java/core/basesyntax/Constant.java
+++ src/test/java/core/basesyntax/Constant.java
@@ -1,22 +1,22 @@
 package core.basesyntax;
 
-public class Constant {
-    public static final String VALID_LOGIN = "123456";
-    public static final String VALID_LOGIN2 = "12345678";
-    public static final String VALID_PASSWORD = "123456";
-    public static final String VALID_PASSWORD2 = "12345678";
-    public static final int VALID_AGE = 18;
-    public static final int VALID_AGE2 = 36;
+public interface Constant {
+    String VALID_LOGIN = "123456";
+    String VALID_LOGIN2 = "12345678";
+    String VALID_PASSWORD = "123456";
+    String VALID_PASSWORD2 = "12345678";
+    int VALID_AGE = 18;
+    int VALID_AGE2 = 36;
 
-    public static final String INVALID_LOGIN = "";
-    public static final String INVALID_LOGIN2 = "123";
-    public static final String INVALID_PASSWORD = "";
-    public static final String INVALID_PASSWORD2 = "123";
-    public static final String INVALID_PASSWORD3 = "12345";
-    public static final int INVALID_AGE = 17;
-    public static final int INVALID_AGE2 = -17;
+    String INVALID_LOGIN = "";
+    String INVALID_LOGIN2 = "123";
+    String INVALID_PASSWORD = "";
+    String INVALID_PASSWORD2 = "123";
+    String INVALID_PASSWORD3 = "12345";
+    int INVALID_AGE = 17;
+    int INVALID_AGE2 = -17;
 
-    public static final int MIN_LENGTH_LOGIN = 6;
-    public static final int MIN_LENGTH_PASSWORD = 6;
-    public static final int MIN_AGE = 18;
+    int MIN_LENGTH_LOGIN = 6;
+    int MIN_LENGTH_PASSWORD = 6;
+    int MIN_AGE = 18;
 }
